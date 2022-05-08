db35ba9aefbafc79780b74d893a9a7cc6c44673a
minhlong149 <minhlong9514@gmail.com>
2022-05-08 14:37:06 +0700

    Tạo form Nganh

---
+++ DangKyHocPhan/Nganh.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace DangKyHocPhan
+{
+    partial class Nganh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMaKhoa = new System.Windows.Forms.TextBox();
+            this.btnThemKhoa = new System.Windows.Forms.Button();
+            this.lblMaKhoa = new System.Windows.Forms.Label();
+            this.btnSuaKhoa = new System.Windows.Forms.Button();
+            this.lblTenKhoa = new System.Windows.Forms.Label();
+            this.btnXoaKhoa = new System.Windows.Forms.Button();
+            this.txtTenKhoa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(484, 426);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtMaKhoa);
+            this.groupBox1.Controls.Add(this.btnThemKhoa);
+            this.groupBox1.Controls.Add(this.lblMaKhoa);
+            this.groupBox1.Controls.Add(this.btnSuaKhoa);
+            this.groupBox1.Controls.Add(this.lblTenKhoa);
+            this.groupBox1.Controls.Add(this.btnXoaKhoa);
+            this.groupBox1.Controls.Add(this.txtTenKhoa);
+            this.groupBox1.Location = new System.Drawing.Point(502, 277);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 161);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin ngành đào tạo";
+            // 
+            // txtMaKhoa
+            // 
+            this.txtMaKhoa.Location = new System.Drawing.Point(103, 30);
+            this.txtMaKhoa.Name = "txtMaKhoa";
+            this.txtMaKhoa.Size = new System.Drawing.Size(177, 22);
+            this.txtMaKhoa.TabIndex = 2;
+            // 
+            // btnThemKhoa
+            // 
+            this.btnThemKhoa.Location = new System.Drawing.Point(211, 123);
+            this.btnThemKhoa.Name = "btnThemKhoa";
+            this.btnThemKhoa.Size = new System.Drawing.Size(69, 23);
+            this.btnThemKhoa.TabIndex = 5;
+            this.btnThemKhoa.Text = "Thêm";
+            this.btnThemKhoa.UseVisualStyleBackColor = true;
+            // 
+            // lblMaKhoa
+            // 
+            this.lblMaKhoa.AutoSize = true;
+            this.lblMaKhoa.Location = new System.Drawing.Point(10, 33);
+            this.lblMaKhoa.Name = "lblMaKhoa";
+            this.lblMaKhoa.Size = new System.Drawing.Size(75, 17);
+            this.lblMaKhoa.TabIndex = 1;
+            this.lblMaKhoa.Text = "Mã ngành:";
+            // 
+            // btnSuaKhoa
+            // 
+            this.btnSuaKhoa.Location = new System.Drawing.Point(136, 123);
+            this.btnSuaKhoa.Name = "btnSuaKhoa";
+            this.btnSuaKhoa.Size = new System.Drawing.Size(69, 23);
+            this.btnSuaKhoa.TabIndex = 6;
+            this.btnSuaKhoa.Text = "Sửa";
+            this.btnSuaKhoa.UseVisualStyleBackColor = true;
+            // 
+            // lblTenKhoa
+            // 
+            this.lblTenKhoa.AutoSize = true;
+            this.lblTenKhoa.Location = new System.Drawing.Point(10, 61);
+            this.lblTenKhoa.Name = "lblTenKhoa";
+            this.lblTenKhoa.Size = new System.Drawing.Size(81, 17);
+            this.lblTenKhoa.TabIndex = 3;
+            this.lblTenKhoa.Text = "Tên ngành:";
+            // 
+            // btnXoaKhoa
+            // 
+            this.btnXoaKhoa.Location = new System.Drawing.Point(61, 123);
+            this.btnXoaKhoa.Name = "btnXoaKhoa";
+            this.btnXoaKhoa.Size = new System.Drawing.Size(69, 23);
+            this.btnXoaKhoa.TabIndex = 7;
+            this.btnXoaKhoa.Text = "Xóa";
+            this.btnXoaKhoa.UseVisualStyleBackColor = true;
+            // 
+            // txtTenKhoa
+            // 
+            this.txtTenKhoa.Location = new System.Drawing.Point(103, 58);
+            this.txtTenKhoa.Name = "txtTenKhoa";
+            this.txtTenKhoa.Size = new System.Drawing.Size(177, 22);
+            this.txtTenKhoa.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Thuộc khoa:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(103, 86);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(177, 24);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // Nganh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Nganh";
+            this.Text = "Quản lý ngành đào tạo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMaKhoa;
+        private System.Windows.Forms.Button btnThemKhoa;
+        private System.Windows.Forms.Label lblMaKhoa;
+        private System.Windows.Forms.Button btnSuaKhoa;
+        private System.Windows.Forms.Label lblTenKhoa;
+        private System.Windows.Forms.Button btnXoaKhoa;
+        private System.Windows.Forms.TextBox txtTenKhoa;
+    }
+}
