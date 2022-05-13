f140feb44173113d661b8aa0f64b9b6837dcf056
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-05-13 15:14:14 +0700

    added Phiếu thu học phí

---
+++ DangKyHocPhan/PhieuThuHocPhi.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace DangKyHocPhan
+{
+    partial class PhieuThuHocPhi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_them = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.text_so_phieu = new System.Windows.Forms.TextBox();
+            this.text_mssv = new System.Windows.Forms.TextBox();
+            this.text_so_tien_thu = new System.Windows.Forms.TextBox();
+            this.date_picker = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_them
+            // 
+            this.button_them.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.button_them.AutoSize = true;
+            this.button_them.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button_them.Location = new System.Drawing.Point(598, 5);
+            this.button_them.Margin = new System.Windows.Forms.Padding(0);
+            this.button_them.MinimumSize = new System.Drawing.Size(180, 50);
+            this.button_them.Name = "button_them";
+            this.button_them.Size = new System.Drawing.Size(180, 57);
+            this.button_them.TabIndex = 1;
+            this.button_them.Text = "THÊM";
+            this.button_them.UseVisualStyleBackColor = false;
+            this.button_them.Click += new System.EventHandler(this.button_them_Click);
+            // 
+            // title
+            // 
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.title.AutoEllipsis = true;
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.title.Location = new System.Drawing.Point(3, 0);
+            this.title.Name = "title";
+            this.title.Padding = new System.Windows.Forms.Padding(5);
+            this.title.Size = new System.Drawing.Size(794, 73);
+            this.title.TabIndex = 0;
+            this.title.Text = "PHIẾU THU HỌC PHÍ";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.title, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 498);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint_1);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(396, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.MinimumSize = new System.Drawing.Size(180, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "HỦY";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.20271F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.7973F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 201F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button_them, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 417);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 67);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox4);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 335);
+            this.panel1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.text_so_phieu);
+            this.groupBox1.Location = new System.Drawing.Point(9, 19);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Số phiếu";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.date_picker);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(441, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(337, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ngày lập";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.text_mssv);
+            this.groupBox3.Location = new System.Drawing.Point(9, 173);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(383, 100);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mã số sinh viên";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.text_so_tien_thu);
+            this.groupBox4.Location = new System.Drawing.Point(441, 173);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(337, 100);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Số tiền thu";
+            // 
+            // text_so_phieu
+            // 
+            this.text_so_phieu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_so_phieu.Location = new System.Drawing.Point(3, 26);
+            this.text_so_phieu.Multiline = true;
+            this.text_so_phieu.Name = "text_so_phieu";
+            this.text_so_phieu.Size = new System.Drawing.Size(377, 71);
+            this.text_so_phieu.TabIndex = 0;
+            // 
+            // text_mssv
+            // 
+            this.text_mssv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_mssv.Location = new System.Drawing.Point(3, 26);
+            this.text_mssv.Multiline = true;
+            this.text_mssv.Name = "text_mssv";
+            this.text_mssv.Size = new System.Drawing.Size(377, 71);
+            this.text_mssv.TabIndex = 1;
+            // 
+            // text_so_tien_thu
+            // 
+            this.text_so_tien_thu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_so_tien_thu.Location = new System.Drawing.Point(3, 26);
+            this.text_so_tien_thu.Multiline = true;
+            this.text_so_tien_thu.Name = "text_so_tien_thu";
+            this.text_so_tien_thu.Size = new System.Drawing.Size(331, 71);
+            this.text_so_tien_thu.TabIndex = 1;
+            // 
+            // date_picker
+            // 
+            this.date_picker.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.date_picker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.date_picker.Location = new System.Drawing.Point(3, 26);
+            this.date_picker.Name = "date_picker";
+            this.date_picker.Size = new System.Drawing.Size(331, 30);
+            this.date_picker.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 71);
+            this.textBox1.TabIndex = 3;
+            // 
+            // PhieuThuHocPhi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PhieuThuHocPhi";
+            this.Text = "Phiếu thu học phí";
+            this.Load += new System.EventHandler(this.PhieuThuHocPhi_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_them;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox text_so_phieu;
+        private System.Windows.Forms.TextBox text_so_tien_thu;
+        private System.Windows.Forms.TextBox text_mssv;
+        private System.Windows.Forms.DateTimePicker date_picker;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ DangKyHocPhan/PhieuThuHocPhi.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DangKyHocPhan
+{
+    public partial class PhieuThuHocPhi : Form
+    {
+        public PhieuThuHocPhi()
+        {
+            InitializeComponent();
+        }
+
+        private void PhieuThuHocPhi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_them_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
