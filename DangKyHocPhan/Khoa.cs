e4eb4b04c400a2ebcb4bf2f87a7bcaa1df62b417
minhlong149 <minhlong9514@gmail.com>
2022-05-15 18:35:01 +0700

    Event btn thêm khoa

--- DangKyHocPhan/Khoa.cs
+++ DangKyHocPhan/Khoa.cs
@@ -22,8 +22,8 @@ namespace DangKyHocPhan
         {
             string query = "SELECT * FROM dbo.KHOA";
 
-            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DKHP.mdf;Integrated Security=True;Connect Timeout=30";
-            SqlConnection connection = new SqlConnection(connectionString);
+            //string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DKHP.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.DKHPConnectionString);
             connection.Open();
             SqlCommand command = new SqlCommand(query, connection);
             DataTable dataTable = new DataTable();
@@ -50,10 +50,12 @@ namespace DangKyHocPhan
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // TODO: Kiểm tra dữ liệu nhập  vào!!
+            
             // Add
             string query = "INSERT INTO dbo.KHOA VALUES (@MaKhoa, @TenKhoa)";
-            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DKHP.mdf;Integrated Security=True;Connect Timeout=30";
-            SqlConnection connection = new SqlConnection(connectionString);
+            //string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DKHP.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection connection = new SqlConnection(Properties.Settings.Default.DKHPConnectionString);
             connection.Open();
             SqlCommand command = new SqlCommand(query, connection);
             command.Parameters.AddWithValue("@MaKhoa", txtMaKhoa.Text);
@@ -62,11 +64,18 @@ namespace DangKyHocPhan
 
             // Refresh
             query = "SELECT * FROM dbo.KHOA";
+            //connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DKHP.mdf;Integrated Security=True;Connect Timeout=30";
+            // connection = new SqlConnection(Properties.Settings.Default.DKHPConnectionString);
             command = new SqlCommand(query, connection);
             DataTable dataTable = new DataTable();
             dataTable.Load(command.ExecuteReader());
             connection.Close();
             dgvDSKhoa.DataSource = dataTable;
+
+            /*
+             * Known bug: Mỗi lần rebuild lại project, dữ liệu nhập trước đó sẽ bị mất
+             * https://www.sentryone.com/blog/aaronbertrand/bad-habits-attachdbfilename
+             */
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
